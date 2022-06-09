2bf866bf42db02f1b435edeca74142807fcc0885
Ильин <discegaudere113399@gmail.com>
2022-06-09 18:38:01 +0300

    находит квадрат заданного числа

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Число: " + a);
+Console.WriteLine("Квадрат: " + a*a);
+
+
+ 
